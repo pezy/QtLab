2b36b6fc35d53ed420daa8af365d39460734e65a
pezy <urbancpz@gmail.com>
2014-05-12 19:17:59 +0800

    add Spreadsheet

---
+++ Spreadsheet/gotocelldialog.cpp
@@ -0,0 +1,25 @@
+#include "gotocelldialog.h"
+#include <QtGui>
+
+GoToCellDialog::GoToCellDialog(QWidget *parent)
+	: QDialog(parent)
+{
+	setupUi(this);
+	buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
+
+	QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
+	lineEdit->setValidator(new QRegExpValidator(regExp, this));
+
+	connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+	connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+GoToCellDialog::~GoToCellDialog()
+{
+
+}
+
+void GoToCellDialog::on_lineEdit_textChanged()
+{
+	buttonBox->button(QDialogButtonBox::Ok)->setEnabled(lineEdit->hasAcceptableInput());
+}
