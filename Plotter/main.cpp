7990ab5f66074fcba1ba24b9c5e57c47d9da4efb
pezy <urbancpz@gmail.com>
2014-05-15 18:17:45 +0800

    finish Plotter

--- Plotter/main.cpp
+++ Plotter/main.cpp
@@ -1,9 +1,11 @@
 
 #include <QtGui/QApplication>
+#include "plotter.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-
+	Plotter plotter;
+	plotter.show();
 	return a.exec();
 }
