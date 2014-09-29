36a9d4e9ead6b76d47cc1010629e3bbca9be2670
pezy <urbancpz@gmail.com>
2014-09-29 19:12:36 +0800

    Draw the static coordinate.

--- QtLab/Widget/widget.cpp
+++ QtLab/Widget/widget.cpp
@@ -1,6 +1,7 @@
 #include "widget.h"
 #include <QDebug>
 #include <QPainter>
+#include <QFontMetrics>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -14,20 +15,52 @@ Widget::~Widget()
 
 void Widget::paintEvent(QPaintEvent *event)
 {
+	QPainter painter(this);
+	
 	/*!
 	 * Draw the border
 	 */
-	QPainter painter(this);
-	QRect rect = this->geometry();
-	rect.moveTopLeft(QPoint(0, 0));
-	rect.adjust(10, 10, -10, -10);
-	qDebug() << rect.x() << "|" << rect.y() << "|" << rect.bottom() << "," << rect.top() << "," << rect.left() << "," << rect.right();
-	painter.drawRect(rect);
+	painter.drawRect(0, 0, width()-1, height()-1);	/**< if you don't subtract 1, you can not see the border on bottom and right. */
+	painter.fillRect(0, 0, width()-1, height()-1, Qt::white);
 
 	/*!
-	 * Draw the slip
+	 * Build the coordinate
 	 */
-	painter.setPen(QPen(Qt::red, 2));
-	painter.drawLine(QPoint(rect.left()+5, rect.top()), QPoint(rect.left()+5, rect.bottom()));
-	painter.drawLine(QPoint(rect.right()-5, rect.top()), QPoint(rect.right()-5, rect.bottom()));
+
+	QFontMetrics metrics = painter.fontMetrics();
+	
+	int leftBearing = metrics.width("100") + 5; /**< 5 is the scale width. */
+	int coordWidth = width() - 2*leftBearing;
+	int coordHeight = height() - 4*metrics.height(); /**< just try */
+
+	QStringList monthList;
+	monthList << "Jan" << "Feb" << "Mar" << "Apr" << "May" << "Jun";
+
+	QStringList valueList;
+	valueList << "0.0" << "3.2" << "6.5" << "9.8" << "13.0";
+
+	painter.translate(leftBearing, coordHeight + 1.75*metrics.height()); /**< move center to left bottom */
+	float deltaX = static_cast<float>(coordWidth)/monthList.size();
+	float deltaY = static_cast<float>(coordHeight)/(valueList.size()-1);
+
+	/*!
+	 * Draw the coordinate
+	 */
+	painter.drawLine(0, 0, coordWidth, 0);
+	for (int i = 0; i != monthList.size(); ++i)
+	{
+		int strLen = metrics.width(monthList.at(i));
+		// scale
+		painter.drawLine(deltaX*(i+1), 0, deltaX*(i+1), 4);
+		// text
+		painter.drawText(deltaX*i + (deltaX-strLen)/2 ,metrics.height(), monthList.at(i));
+	}
+
+	painter.drawLine(0, 0, 0, -coordHeight);
+	for (int i = 0; i != valueList.size(); ++i)
+	{
+		int deviation = metrics.height()/2 - metrics.descent();
+		painter.drawLine(-4, -deltaY*i, 0, -deltaY*i);
+		painter.drawText(-metrics.width(valueList.at(i))-4, -deltaY*i+deviation, valueList.at(i));
+	}
 }
