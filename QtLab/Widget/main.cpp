17c4098c0198f8a2d2e6fc72dafd64128fecca2b
pezy <urbancpz@gmail.com>
2014-10-09 00:37:53 +0800

    Added data value set and color tips on the chart.

--- QtLab/Widget/main.cpp
+++ QtLab/Widget/main.cpp
@@ -1,11 +1,38 @@
 #include <QApplication>
-#include "widget.h"
+#include <QMainWindow>
+#include "qchart.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    
+	QBarSet *set0 = new QBarSet("Jane", Qt::darkCyan);
+	QBarSet *set1 = new QBarSet("John", Qt::darkGreen);
+	QBarSet *set2 = new QBarSet("Axel", Qt::darkYellow);
+	QBarSet *set3 = new QBarSet("Mary", Qt::darkMagenta);
+	QBarSet *set4 = new QBarSet("Samantha", Qt::darkRed);
+
+	*set0 << 1 << 2 << 3 << 4 << 5 << 6;
+	*set1 << 5 << 0 << 0 << 4 << 0 << 7;
+	*set2 << 3 << 5 << 8 << 13 << 8 << 5;
+	*set3 << 5 << 6 << 7 << 3 << 4 << 5;
+	*set4 << 9 << 7 << 5 << 3 << 1 << 2;
+
+	QBarSeries *series = new QBarSeries();
+	series->append(set0);
+	series->append(set1);
+	series->append(set2);
+	series->append(set3);
+	series->append(set4);
+
+	QChart *chart = new QChart;
+	chart->setTitle("Simple barchart example");
+	chart->addSeries(series);
+	
+	QMainWindow window;
+	window.setCentralWidget(chart);
+	window.resize(420, 300);
+	window.show();
 	
     return a.exec();
 }
---
+++ QtLab/Widget/qchart.h
@@ -0,0 +1,64 @@
+#ifndef QCHART_H
+#define QCHART_H
+
+#include <QWidget>
+#include <QSet>
+
+/*!
+ * \class QBarSet
+ *
+ * \brief The value set of the chart.
+ *
+ * \author pezy
+ * \date Oct. 2014
+ */
+
+class QBarSet : public QSet<int>
+{
+public:
+	QBarSet(const QString &name, const QColor &color):_name(name),_color(color) {}
+	~QBarSet(){}
+	QString name() const { return _name; }
+	QColor color() const { return _color; }
+
+private:
+	QString _name;
+	QColor _color;
+};
+
+typedef QVector<QBarSet*> QBarSeries;
+
+/*!
+ * \class QChart
+ *
+ * \brief Simple custom chart widget.
+ *
+ * \author PEZY
+ * \date Sept. 2014
+ */
+
+class QChart : public QWidget
+{
+    Q_OBJECT
+
+	Q_PROPERTY(QString title READ title WRITE setTitle);
+    
+public:
+    QChart(QWidget *parent = 0);
+    ~QChart();
+
+	QString title() const { return _title; }
+	void setTitle(const QString &title) { _title = title; }
+
+	void addSeries(QBarSeries *series) { _series = series; }
+
+protected:
+	virtual void paintEvent(QPaintEvent *event);
+	virtual void wheelEvent(QWheelEvent *event);
+
+private:
+	QString _title;
+	QBarSeries *_series;
+};
+
+#endif // QCHART_H
