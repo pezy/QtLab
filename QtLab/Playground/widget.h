fd0e90c0af9330da40c0309955c95948270a499b
pezy <urbancpz@gmail.com>
2014-10-22 15:29:05 +0800

    fixed #10

--- QtLab/Playground/widget.h
+++ QtLab/Playground/widget.h
@@ -8,27 +8,43 @@
 #include <QMenu>
 #include <QObject>
 #include <QColorDialog>
+#include <QLinearGradient>
+#include <QPropertyAnimation>
+#include <QEasingCurve>
 
 class Widget : public QWidget
 {
     Q_OBJECT
 
 public:
-    Widget():m_pos(-1),m_dragging(false),m_deleted(false),m_color(Qt::red)
+    Widget():m_pos(-1),m_dragging(false),m_deleted(false),m_color(Qt::white)
     {
     setMouseTracking(true);
         m_meun = new QMenu;
         QAction *changeColorAction = new QAction(tr("change color"), this);
         QAction *delAction = new QAction(tr("delete"), this);
+		m_curColor = new QAction(tr("RGB()"), this);
         m_meun->addAction(changeColorAction);
         m_meun->addAction(delAction);
+		m_meun->addAction(m_curColor);
         connect(delAction, SIGNAL(triggered()), this, SLOT(slotDel()));
 		connect(changeColorAction, SIGNAL(triggered()), this, SLOT(slotColorChanged()));
     }
 
+protected:
     virtual void paintEvent(QPaintEvent *)
     {
         QPainter painter(this);
+
+		// add QLinearGradient
+		QLinearGradient linearGradient(QPoint(rect().left(), rect().height()/2), QPoint(rect().right(), rect().height()/2));
+		linearGradient.setColorAt(1, Qt::red);
+		linearGradient.setColorAt(0.5, Qt::green);
+		linearGradient.setColorAt(0, Qt::blue);
+
+		painter.setPen(QPen(QBrush(linearGradient), rect().height()));
+		painter.drawLine(QPoint(rect().left(), rect().height()/2), QPoint(rect().right(), rect().height()/2));
+
         painter.setPen(QPen(QBrush(m_color), 3));
         if (m_pos == -1) m_pos = rect().left() + 100;
         if (!m_deleted)
@@ -53,6 +69,11 @@ public:
     {
         if (event->pos().x() >= m_pos-1 && event->pos().x() <= m_pos+1)
             m_dragging = true;
+
+		if (event->button() == Qt::RightButton)
+		{
+			m_curColor->setText(getColor(static_cast<qreal>(event->pos().x())/rect().width()).name());
+		}
     }
 
     virtual void mouseReleaseEvent(QMouseEvent *)
@@ -90,11 +111,26 @@ private slots:
 	}
 
 private:
+	QColor getColor(qreal pos) const
+	{
+		QPropertyAnimation interpolator;
+		const qreal granularite = 100.0;
+		interpolator.setEasingCurve(QEasingCurve::Linear);
+		interpolator.setDuration(granularite);
+		interpolator.setKeyValueAt(0, Qt::blue);
+		interpolator.setKeyValueAt(0.5, Qt::green);
+		interpolator.setKeyValueAt(1, Qt::red);
+		interpolator.setCurrentTime(pos * granularite);
+		return interpolator.currentValue().value<QColor>();
+	}
+
+private:
     int m_pos;
     bool m_dragging;
     bool m_deleted;
     QMenu *m_meun;
 	QColor m_color;
+	QAction *m_curColor;
 };
 
 #endif
